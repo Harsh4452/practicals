9c9a81259963ac90f9beb3e5b63dfe311e5420c8
Harsh4452 <126318301+Harsh4452@users.noreply.github.com>
2023-12-12 18:41:15 +0530

    Update employee.cpp

--- oops/employee.cpp
+++ oops/employee.cpp
@@ -38,7 +38,13 @@ int main()
  { 
  cout<<"\n Enter information of Employee "<<i+1<<"\n"; 
  o[i].accept(); 
- f.write((char*)&o[i],sizeof o[i]); 
+ f.write((char*)&o[i],sizeof o[i]);   /*&obj - this is the memory address of the object called obj.
+
+(char*) &obj - you are type casting &obj into a character pointer.
+
+sizeof(obj) - yields the size of the object that you are referring to.
+
+fin.read - this is the function call evoked by the fin object.*/
  } 
  
  f.close(); 
